1c278de64fff6805bc4700a3bc2fad454696395a
Ruben Garcia <rbn@leetmaster.local>
2018-07-11 16:42:34 -0500

    Goliath

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     # Eternal posting:
     while True:
         # Extract status:
-        status = extract_status("cyberia1.txt")
+        status = extract_status("goliath.txt")
         print(status)
 
         # Try to post status:
